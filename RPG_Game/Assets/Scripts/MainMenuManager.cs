e52ffbefecd79407246f432b5cbce5216cbecd45
Suraj Singh <surajss@uci.edu>
2020-02-27 11:57:17 -0800

    Day 1 finished

---
+++ RPG_Game/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public int mainMenuNumber;
+    public int gameLevelNumber;
+
+    public void GoToGame()
+    {
+        SceneManager.LoadScene(gameLevelNumber);
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene(mainMenuNumber);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadGame()
+    {
+        // TO DO NEXT WEEK
+    }
+}
